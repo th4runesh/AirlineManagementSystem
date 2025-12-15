cfe65d2c0b7adf0aa587bcf65106ea1067621cb4
th4run2006 <th4run2006@gmail.com>
2025-12-16 01:09:15 +0530

    Added Conn module

---
+++ AirlineManagementSystem/src/Conn.java
@@ -0,0 +1,23 @@
+package airlinemanagementsystem;
+
+import java.sql.*;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+
+    public Conn() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/airline",
+                "root",
+                "root"
+            );
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
